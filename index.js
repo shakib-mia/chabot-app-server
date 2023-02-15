e66520acc202036d9d56a45bbf21dd970cb4b7d8
shakib-mia <smdshakibmia2001@gmail.com>
2023-02-15 11:08:02 +0600

    db added

--- index.js
+++ index.js
@@ -11,19 +11,39 @@ const port = process.env.PORT || 4000;
 const { Configuration, OpenAIApi } = require("openai");
 
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@cluster0.ylyrso9.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.get('/', async (req, res) => {
     // console.log(req.body);
 
     res.send(`from port ${port}`)
 })
 
+app.get("/messages", async (req,res) => {
+    const query = {};
+
+    const collection = client.db('chatbot').collection('messages')
+    const cursor = collection.find(query);
+    const messages = await cursor.toArray();
+
+    res.send(messages)
+})
+
 app.get("/query/:data", async (req, res) => {
     const configuration = new Configuration({
-        apiKey: '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',
+        apiKey: process.env.OPENAI_API_KEY,
     });
     const openai = new OpenAIApi(configuration);
 
     try {
+        await client.connect();
+
+        const collection = client.db('chatbot').collection('messages')
+
         const completion = await openai.createCompletion({
             model: "text-davinci-002",
             prompt: req.params.data,
@@ -33,6 +53,13 @@ app.get("/query/:data", async (req, res) => {
             frequency_penalty: 0.5,
             presence_penalty: 0,
         });
+        const data = {
+            message: req.params.data,
+            reply: completion.data.choices[0].text
+        };
+        const cursor = collection.insertOne(data);
+        // const reply = cursor.toArray()
+
         res.send(completion.data.choices[0]);
     } catch (error) {
         if (error.response) {
