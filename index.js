784d66add50f61a13ad7f1ff5390f7cc6ce5e943
shakib-mia <smdshakibmia2001@gmail.com>
2023-02-18 21:13:16 +0600

    get users added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 const { Configuration, OpenAIApi } = require("openai");
+const jwt = require('jsonwebtoken');
 
 app.use(cors())
 app.use(express.json())
@@ -62,6 +63,43 @@ app.post("/query", async (req, res) => {
         } else {
             console.log(error.message);
         }
+    }
+})
+
+app.get('/users/:email/:pass', async (req, res) => {
+    const query = { email: req.params.email, pass: req.params.pass };
+    const collection = client.db('chatbot').collection('users')
+    const cursor = collection.find(query);
+    const users = await cursor.toArray();
+
+    res.send(users)
+
+    // console.log(users);
+})
+
+app.get('/users', async (req, res) => {
+    const query = {}
+    const collection = client.db('chatbot').collection('users')
+    const cursor = collection.find(query);
+    const users = await cursor.toArray();
+    res.send(users)
+})
+
+app.post('/users', async (req, res) => {
+    const { body } = req;
+    const collection = client.db('chatbot').collection('users');
+
+
+    const userCursors = collection.find({ email: body.email })
+    const users = await userCursors.toArray()
+    // console.log(users.length);
+
+    if (users.length === 0) {
+        const cursor = await collection.insertOne(body);
+        res.send(cursor)
+    } else {
+        res.send({ message: 'User Already Exists' })
+        res.status(409)
     }
 })
 
